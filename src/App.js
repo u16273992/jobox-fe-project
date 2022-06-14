59e17cc46fe589f9a1799257b812005361172f86
u16273992 <u16273992@tuks.co.za>
2022-06-14 23:57:58 +0200

    added button, UI done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Contract from './components/contract';
 
+
 function App() {
+
+  function handleSubmit(e) {
+    e.preventDefault();    
+    console.log('You clicked submit.');
+  }
+
   return (
     <div style={styles.background}>
       <Contract/>
+      <button onClick={handleSubmit} style={styles.button}>
+        DOWNLOAD CONTRACT
+      </button>
     </div>
     
   );
@@ -16,11 +26,19 @@ const styles = {
   background:{
     backgroundColor: "#f8f9fa",
   },
-  heading: {
-
+  buttonView: {
+    marginTop: "2cm",
+    marginBottom: "2cm",
+    display:"flex",
+    flexDirection:"column",
+    alignItems:"center",
+    justifyContent:"center",
   },
-  body: {
-
-  },
+  button: {
+    position: "fixed",
+    bottom: "0.5rem",
+    right: "0.5rem",
+    boxShadow: "0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)"
+  }
 
 }
