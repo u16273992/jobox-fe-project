4987258abb66c88816437a36cac7b7d9f1671081
u16273992 <u16273992@tuks.co.za>
2022-06-20 19:53:08 +0200

    pages refractoring

--- src/components/contract.js
+++ src/components/contract.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Page from './page';
 
 export default function Contract() {
+
+  let pages = [1,2,3];
+
   return (
     <div id ="downloadContract">
-        <Page number={1}/>
-        <Page number={2}/>
-        <Page number={3}/>
+        {
+          pages.map((item,index)=>{
+            return <Page key = {index} number={item}/>
+          })
+        }
+
     </div>
   )
 }
